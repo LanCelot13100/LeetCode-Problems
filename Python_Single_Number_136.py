35f8bff46659ff07916918861b022a699f6f87de
Or1on <lancelot25565@gmail.com>
2025-06-27 16:44:49 +0300

    Add files via upload

---
+++ Python_Single_Number_136.py
@@ -0,0 +1,33 @@
+
+#  Here is the solution of the 136. Single Number LeetCode problem (Python)
+#  I was able to come up with my own way of solving that. Here it is:
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #  Here is the solution:
+        new_list = []
+        bad_list = []
+
+        for row in nums:
+            if row in new_list:
+                bad_list.append(row)
+                continue
+            new_list.append(row)
+
+        for row2 in bad_list:
+            while row2 in nums:
+                nums.remove(row2)
+
+        number = nums[0]
+        return number
+
+
+#  This block of code down below is needed to run the program in your IDE, but you don't actually need it when you check your answer on LeetCode
+numbers = [2,2,1]
+
+variable = Solution()
+print(variable.singleNumber(nums=numbers))
